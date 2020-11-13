33f2f7574406ff4b88255be2356cb4fe7015545a
JDugonjic <jelenadugonjic@gmail.com>
2020-11-13 16:13:04 +0100

    Second commit - added Nav and Results

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
+import Nav from "./Nav";
+import requests from "./requests";
+import Results from "./Results";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending);
+
   return (
     <div className="app">
       <Header />
 
-      {/*Nav*/}
+      <Nav setSelectedOption={setSelectedOption}/>
 
-      {/*Results*/}
+      <Results selectedOption={selectedOption} />
     </div>
   );
 }
